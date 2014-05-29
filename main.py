e8aeb7c68ec951445458b12b124f9e2a112f2167
andy88r <andy@andys-macbook.default>
2014-05-29 21:14:13 +0100

    Made bezier lines appear
    Logic works for points on the line.
    Need to make it so 1 line appears and automatically updates instead of
    lots of lines appearing

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
 #!/usr/bin/env python
+
+#scren size is 800. have between 0 and 800 with 100 intervals for x. randomise y instaed of adding on.
+#make a function which just updates the line with the new points each time
+
 from kivy.app import App
 from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.slider import Slider
+from kivy.uix.widget import Widget
+from kivy.core.window import Window
+from kivy.clock import Clock
 from kivy.graphics import Color, Bezier, Line
+import random
 
 class BezierLine(FloatLayout):
 
@@ -20,25 +28,58 @@ class BezierLine(FloatLayout):
                     segments=180,
                     loop=self.loop,
                     )
+ 
+class HelicopterGame(Widget):
+    x_list=[]
+    y_list=[]
 
-                 
+    def __init__(self, **kw):
+        super(HelicopterGame, self).__init__(**kw)
+
+        self.x_list=range(0,900,100)
+        self.y_list=self.generate_random_list()
+
+        Clock.schedule_interval(self.update, 1)
+
+    def generate_random_list(self):
+        y=[]
+        for i in range(0,9,1):
+            y.extend([random.randint(200,400)])
+        return y
+
+    def merge_lists(self, x, y):
+        b_list=[]
+        for i in range(len(x)):
+            b_list.extend([x[i]])
+            b_list.extend([y[i]])
+        return b_list
+
+    def flow_y(self,y):
+        y_list=[]
+        for i in range(len(y)-1):
+            y_list.extend([y[i+1]])
+        y_list.extend([random.randint(200,400)])
+        return y_list
+
+    def update(self, dt):
+        #self.y_list=self.generate_random_list()
+        self.y_list=self.flow_y(self.y_list)
+        b_list=self.merge_lists(self.x_list,self.y_list)
+        print b_list
+        #print x_list, y_list
+
+        #self.drawLine()
+
+        #print self.points_all
+        #self.
+        self.add_widget(BezierLine(points=b_list))
+        #return BezierLine(points=b_list)
+
 class TestApp(App):
 
     def build(self):
-        from math import cos, sin, radians
-        x = 0; y = 300
-        d=30
-        points = [x, y]
-        for i in range(0, 10, 1):
-            print i
-            #print y
-            #make a sin wave
-            #y goes 0, -1, 0, -1
-            x=x+100
-            y=y+50
-            points.extend([x, y])
-        points1 = [0,200,100,300,200,200,300,100,400,200,500,300,600,200]
-        return BezierLine(points=points1, loop=False)
+        game = HelicopterGame(size=Window.size)
+        return game
 
 if __name__ == '__main__':
     TestApp().run()
