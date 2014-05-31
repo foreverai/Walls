d98f6e890408fbf67fcecdc20d0817fb92541ae0
andy88r <andy@andys-macbook.default>
2014-05-31 13:08:02 +0100

    Not fully working background scrolling and bezier
    Ian please fix the background. The main background doesn’t appear and i
    don’t know why.
    Speeds also need adjusting.
    Next Aim for me is to make one line appear and update smoothly.

--- walls.py
+++ walls.py
@@ -3,6 +3,9 @@
 #scren size is 800. have between 0 and 800 with 100 intervals for x. randomise y instaed of adding on.
 #make a function which just updates the line with the new points each time
 
+import kivy
+kivy.require('1.8.0')
+
 from kivy.app import App
 from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.slider import Slider
@@ -11,8 +14,13 @@ from kivy.core.window import Window
 from kivy.clock import Clock
 from kivy.graphics import Color, Bezier, Line
 import random
+from kivy.properties import NumericProperty, ReferenceListProperty,\
+    ObjectProperty
+from kivy.core.image import Image as CoreImage
+from kivy.graphics import Rectangle
+from kivy.uix.label import Label
 
-class BezierLine(FloatLayout):
+class BezierLine(Widget):
 
     def __init__(self, points=[], loop=False, *args, **kwargs):
         super(BezierLine, self).__init__(*args, **kwargs)
@@ -32,14 +40,26 @@ class BezierLine(FloatLayout):
 class HelicopterGame(Widget):
     x_list=[]
     y_list=[]
+    line=ObjectProperty(None)
+    back_scroll_speed = NumericProperty(0.1)
 
     def __init__(self, **kw):
         super(HelicopterGame, self).__init__(**kw)
 
-        self.x_list=range(0,900,100)
-        self.y_list=self.generate_random_list()
+        with self.canvas.before:
+            texture = CoreImage('Images/background.png').texture
+            texture.wrap = 'repeat'
+            self.scroll_back = Rectangle(texture=texture, size=self.size, pos=self.pos)
+            self.x_list=range(0,900,100)
+            self.y_list=self.generate_random_list()
+            Clock.schedule_interval(self.update, 0)
 
-        Clock.schedule_interval(self.update, 1)
+        
+        #Clock.schedule_interval(self.update, 1)
+
+    def scroll_background(self, *l):
+        t = Clock.get_boottime()
+        self.scroll_back.tex_coords = -(t * self.back_scroll_speed), 0, -(t * self.back_scroll_speed + 1), 0,  -(t * self.back_scroll_speed + 1), -1, -(t * self.back_scroll_speed), -1 
 
     def generate_random_list(self):
         y=[]
@@ -62,6 +82,7 @@ class HelicopterGame(Widget):
         return y_list
 
     def update(self, dt):
+        self.scroll_background()
         #self.y_list=self.generate_random_list()
         self.y_list=self.flow_y(self.y_list)
         b_list=self.merge_lists(self.x_list,self.y_list)
